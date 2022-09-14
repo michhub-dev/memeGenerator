ea3fd5603edf8d42da70457e19f111caa4e48d56
Michelle David <rachaelholy@gmail.com>
2022-09-14 17:16:51 +0100

    Defined getMeme func

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -6,12 +6,20 @@ import memeData from './memeData'
 import './App.css'
 
 export default function App() {
-  const [memes, setMemes] = useState(memeData)
+  const [meme, setMemes] = useState(memeData)
+
+  function getMeme(){
+      const data = meme.data.memes
+     const randomData = Math.floor(Math.random() * data.length)
+      const url = data[randomData].url
+      setMemes(url)
+  }
+  const mapArray = meme.map(item => <Main getMeme={getMeme}/>)
 
   return (
     <>
     <Header />
-    <Main />
+    {mapArray}
     </>
   )
 }
--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,5 +1,5 @@
 
-export default function Main(){
+export default function Main(props){
     return(
         <>
          <main className="grid grid-cols-[40px 40px / 1fr 1fr] gap-4 w-50 m-10 h-10 p-6 " >
@@ -14,6 +14,7 @@ export default function Main(){
              className="border p-4 border-grey-500 shadow-md outline-none text-center rounded-md"
              /><br/>
             <button
+            onClick={props.getMeme}
              type="submit" 
              className="h-15 w-[100%] grid-cols-[1 /-1]  text-center rounded-md shadow-md text-white text-lg font-medium bg-gradient-to-r from-sky-500 to-indigo-500">
                 Get new meme
