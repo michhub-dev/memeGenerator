de2d057ec0d6ff6835484a168712a4967850eb5f
Michelle David <rachaelholy@gmail.com>
2022-09-14 13:33:23 +0100

    Created Header component

--- meme-generator/src/components/Header.jsx
+++ meme-generator/src/components/Header.jsx
@@ -1,12 +1,14 @@
 
+import '/images/vector.png'
+
 export default function Header(){
     return (
         <>
-         <header className="bg-red-600 flex items-center justify-between p-10 w-100 h-10">
-            <img src="" alt=""/>
-            <h2>Meme Generator</h2> 
+         <header className="bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center flex-row justify-between p-10 w-100 h-10 mx-auto ">
+            <img src="images/vector.png" alt="logo" className='w-10 h-15  '/>
+            <h2 className='text-xl font-sans text-white font-semibold flex '>Meme Generator</h2> 
             <div>
-                <p>React project - memes </p>
+                <p className='text-white font-mono text-xs'>React project - memes </p>
             </div>
          </header>
         </>
