036744cc9ae515ae3491235a1f96faf37fa7f662
Michelle David <rachaelholy@gmail.com>
2022-09-14 12:28:17 +0100

    first commit

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Header from './components/Header'
 import './App.css'
 
 export default function App() {
- 
+  
 
   return (
     <>
-      
+    <Header/>
     </>
   )
-   
-  }
+}
+
+
---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+export default function Header(){
+    return (
+        <>
+         <header className="bg-red-600 flex items-center justify-between p-10 w-100 h-10">
+            <img src="" alt=""/>
+            <h2>Meme Generator</h2> 
+            <div>
+                <p>React project - memes </p>
+            </div>
+         </header>
+        </>
+    )
+}
