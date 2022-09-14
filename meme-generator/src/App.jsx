4011e38c72697726100082c8d5f10acb66013cda
Michelle David <rachaelholy@gmail.com>
2022-09-14 17:04:46 +0100

    Imported array

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
 import Main from './components/Main'
+import memeData from './memeData'
 import './App.css'
 
 export default function App() {
-  
+  const [memes, setMemes] = useState(memeData)
 
   return (
     <>
