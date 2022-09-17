378123e9996f0b3c6ddc63c9b6280874d5d68497
Michelle David <rachaelholy@gmail.com>
2022-09-17 17:37:37 +0100

    Defined unchange func. Moved all code to main file

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -2,35 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
 import Main from './components/Main'
-import memeData from './memeData'
+
 import './App.css'
 
 export default function App() {
-  const [meme, setMemes] = useState(memeData)
-  const [memeImage, setMemeImage] = useState('')
-  const [memeInfo, setMemeInfo] = useState({
-    topText: "",
-    bottomText: "",
-    imageData: "https://i.imgflip.com/30b1gx.jpg" 
-  })
- 
-
-  //Generate random meme image 
-  function getMeme(){
-      const arrayData = meme.data.memes
-     const randomData = Math.floor(Math.random() * arrayData.length)
-      const url = arrayData[randomData].url
-      setMemeInfo(prevState => ({
-        ...prevState, 
-        imageData: url}))
-  }
-// const mapArray = memeInfo.map(item => <Main getMeme={getMeme} memeInfo={memeInfo.imageData} topText={topText} bottomText={bottomText} />)
-
+  
   return (
     <>
     <Header />
-    <Main getMeme={getMeme} memeInfo={memeInfo.imageData} />
-   {/* {mapArray}topText={topText} bottomText={bottomText}*/}
+    <Main />
     </>
   )
 }
--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,25 +1,63 @@
+import memeData from "../memeData"
+import { useState } from 'react'
 
-export default function Main(props){
+export default function Main(){
+    const [meme, setMemes] = useState(memeData)
+
+    const [memeInfo, setMemeInfo] = useState({
+        topText: "",
+        bottomText: "",
+        imageData: "https://i.imgflip.com/30b1gx.jpg" 
+      })
+
+      //Generate random meme image 
+        function getMeme(){
+            const arrayData = meme.data.memes
+        const randomData = Math.floor(Math.random() * arrayData.length)
+            const url = arrayData[randomData].url
+            setMemeInfo(prevState => ({
+            ...prevState, 
+            imageData: url}))
+        }
+
+        function handleChange(event){
+        const {name, value} = event.target
+        setMemeInfo(prevData => ({
+            ...prevData,
+            [name] : value
+        }))
+}
     return(
         <>
          <main className="grid grid-cols-[40px 40px / 1fr 1fr] gap-4 w-50 m-10 h-10 p-6 " >
             <input
              type="text"
+             name="topText"
+             value={memeInfo.topText}
+             onChange={handleChange}
              placeholder="Top text" 
              className="border p-4 border-grey-500 shadow-md outline-none text-center rounded-md"
              />
             <input
              type="text" 
+             name="bottomText"
+             value={memeInfo.bottomText}
+             onChange={handleChange}
              placeholder="Bottom text" 
              className="border p-4 border-grey-500 shadow-md outline-none text-center rounded-md"
              /><br/>
             <button
-            onClick={props.getMeme}
+             onClick={getMeme}
              type="submit" 
              className="h-15 w-[100%] grid-cols-[1 /-1]  text-center rounded-md shadow-md text-white text-lg font-medium bg-gradient-to-r from-sky-500 to-indigo-500">
                 Get new meme
             </button>
-            <img src={props.memeInfo} />
+            <div>
+            <img src={memeInfo.imageData} />
+            <h2>{memeInfo.topText}</h2>
+            <h2>{memeInfo.bottomText}</h2>
+            </div>
+            
          </main>
         </>
     )
